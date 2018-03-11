5404efb40c7403a3686277b763aa045f49b1e3cd
Piotrek Chmielowski <piotrek.chmielowski@callpage.io>
2018-03-11 20:42:53 +0100

    Draws extra points

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter
 import math
+from random import randint
 def  draw (point):
     canvas.create_oval(point['x'], point['y'], 
     point['x'], point['y'], 
@@ -15,5 +16,14 @@ points=[{'x':80, 'y':200},
     {'x':80+a/2, 'y':200-a*math.sqrt(3)/2}]
 for point in points:
    draw(point)
+
+first=points [randint(0,2)]
+second=points [randint(0,2)]
+
+x=(first['x']+second['x'])/2
+y=(first['y']+second['y'])/2
+current={'x':x, 'y':y}
+
+draw (current)
 
 root.mainloop()
